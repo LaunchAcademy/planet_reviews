0643adf999df73c3a39c7af351c214b5361b9d81
Dan Pickett <dan.pickett@launchacademy.com>
2015-06-30 14:47:27 -0400

    unit tests

--- spec/models/planet_spec.rb
+++ spec/models/planet_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe Planet, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Planet, type: :model do
+  it { should belong_to(:user) }
+
+  it { should have_valid(:name).when("Earth", "Venus", "Jupiter") }
+  it { should_not have_valid(:name).when(nil, "") }
 end

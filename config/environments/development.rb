03f542993cfb5de4d12b5870174ff0760754c7da
Richard Davis <rdavis.bacs@gmail.com>
2015-07-09 12:57:40 -0400

    stuff

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,6 +43,14 @@ Rails.application.configure do
   #   to view emails generated in development:
   #   `gem install mailcatcher && mailcatcher`
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { address: "localhost", port: 1025 }
-  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+
+  config.action_mailer.smtp_settings = {
+    address: "localhost",
+    port: 1025
+  }
+
+  config.action_mailer.default_url_options = {
+    host: "localhost",
+    port: 3000
+  }
 end

58c56ac5814c458567ef27e30fc46a215d7b1956
Richard Davis <rdavis.bacs@gmail.com>
2016-04-25 11:51:35 -0400

    mock_omniauth method

--- spec/features/github_authentication_spec.rb
+++ spec/features/github_authentication_spec.rb
@@ -3,20 +3,25 @@ require "rails_helper"
 feature "github authentication", focus: true do
   scenario "user signs in with github" do
     user = FactoryGirl.build(:user)
-    mock_user_attributes = {
-      uid: user.uid,
-      provider: :github,
-      info: {
-        email: user.email
-      }
-    }
-
-    OmniAuth.config.test_mode = true
-    OmniAuth.config.add_mock(:github, mock_user_attributes)
+    mock_omniauth(user, :github)
 
     visit root_path
     click_link "GitHub"
     expect(page).to have_content("Successfully authenticated from GitHub account.")
     expect(page).to have_link("Sign Out")
   end
+end
+
+def mock_omniauth(user, provider)
+  OmniAuth.config.test_mode = true
+
+  mock_user_attributes = {
+    uid: user.uid,
+    provider: provider,
+    info: {
+      email: user.email
+    }
+  }
+
+  OmniAuth.config.add_mock(:github, mock_user_attributes)
 end
